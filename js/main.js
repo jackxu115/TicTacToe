c9ca08733d52b1339b7a52433eaf6db511594100
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-08-01 00:30:28 -0700

    js gameboard, play, and winning condition

--- js/main.js
+++ js/main.js
@@ -6,10 +6,15 @@ const container = document.querySelector(".container")
 const restartBtn = document.querySelector("#restartBtn")
 const gamePage = document.querySelector(".container_gamePage")
 const gameItem = document.querySelectorAll(".container_gamePage_gameArea_item")
+const gameDot = document.querySelectorAll(".dot")
+const playerOneTag = document.querySelector("#playerOne")
+const playerTwoTag = document.querySelector("#playerTwo")
 
 let GameChoice = null
 let PlayerOne = []
 let PlayerTwo = []
+let turns = 0
+const winningArray = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 const activeGameChoice = (evtActive, evtDisable) => {
     if (evtActive.style.backgroundColor === "") {
@@ -39,10 +44,12 @@ startBtn.addEventListener('click', () => {
         console.log('pvc game')
         gamePage.setAttribute('style', 'visibility: visible')
         startPage.setAttribute('style', 'visibility: hidden')
+        GameBoard.pvpPlay()
     } else if (GameChoice === 'pvpBtn') {
         console.log('pvp game')
         gamePage.setAttribute('style', 'visibility: visible')
         startPage.setAttribute('style', 'visibility: hidden')
+        GameBoard.pvpPlay()
     } else {
         alert('please select one of game before start the game')
     }
@@ -51,6 +58,8 @@ startBtn.addEventListener('click', () => {
 restartBtn.addEventListener('click', () => {
     gamePage.setAttribute('style', 'visibility: hidden')
     startPage.setAttribute('style', 'visibility: visible')
+    gameDot.forEach(key => key.textContent = "")
+    turns = 0
 })
 
 
@@ -59,31 +68,54 @@ const computerPlay = () => {
 }
 
 const GameBoard = (() => {
-    const playerOnePlay = () => {
+    const pvpPlay = () => {
+        playerOneTag.setAttribute('style', 'border: 2px #00abd5 solid')
         gameItem.forEach(key => key.addEventListener('click', event => {
-                    const id = event.target.children[0].id
-                    const input = document.getElementById(`${id}`)
+                const id = event.target.children[0].id
+                const input = document.getElementById(`${id}`)
+                const index = id.charAt(3)
+                console.log(turns)
+                if (turns % 2 === 0) {
                     input.textContent = "X"
-                }, {once: true}
-            )
-        )
-    }
-
-    const playerTwoPlay = () => {
-        gameItem.forEach(key => key.addEventListener('click', event => {
-                    const id = event.target.children[0].id
-                    const input = document.getElementById(`${id}`)
+                    playerOneTag.setAttribute('style', 'border: none')
+                    playerTwoTag.setAttribute('style', 'border: 2px #00abd5 solid')
+                    PlayerOne.push(index)
+                    console.log(PlayerOne)
+                } else {
                     input.textContent = "O"
+                    playerOneTag.setAttribute('style', 'border: 2px #00abd5 solid')
+                    playerTwoTag.setAttribute('style', 'border: none')
+                    PlayerTwo.push(index)
+                    console.log(PlayerTwo)
+                }
+                turns++
                 }, {once: true}
             )
         )
     }
 
+    const winningCondition = (playerArray) => {
+        // playerArray.sort()
+        for (let index in winningArray) {
+            // console.log(winningArray[index])
+            if (playerArray.some(i => winningArray[index].includes(i))) {
+                return true
+            }
+        }
 
+    }
 
-    return {playerOnePlay, playerTwoPlay}
+    return {pvpPlay, winningCondition}
 
 })()
 
-GameBoard.playerOnePlay()
-GameBoard.playerTwoPlay()
+play = [1, 8, 5, 2, 6, 9]
+
+for (let index in winningArray) {
+    // console.log(winningArray[index])
+    if (winningArray[index].every(element => play.includes(element))) {
+        console.log('win', winningArray[index])
+    } else {
+        console.log('lose')
+    }
+}
